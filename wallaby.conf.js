2017319cf89ac3a83ca18fda45d41be924390d72
Jan Jorgensen <jandjorgensen@gmail.com>
2019-02-03 23:45:05 -0700

    rule and ruleset

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,19 +1,19 @@
-module.exports = function (wallaby) {
+module.exports = function(wallaby) {
   return {
-    files: [
-      'lib/**/*.js',
-      { pattern: 'lib/**/*.test.js', ignore: true }
-    ],
-    tests: [
-      'lib/**/*.test.js'
-    ],
-    env: {
-      type: 'node'
-    },
+    files: ['lib/**/*.js', { pattern: 'lib/**/*.test.js', ignore: true }],
+    tests: ['lib/**/*.test.js'],
     compilers: {
       'lib/**/*.js': wallaby.compilers.babel({
-        presets: ['env']
+        presets: ['@babel/preset-env'],
+        plugins: [
+          '@babel/plugin-transform-runtime',
+          '@babel/plugin-syntax-object-rest-spread',
+          '@babel/plugin-proposal-class-properties'
+        ]
       })
+    },
+    env: {
+      type: 'node'
     },
     testFramework: 'jest',
     debug: true
